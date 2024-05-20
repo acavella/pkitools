63b535721bdb0041b8d803a89c1eb8f7126d50e1
Tony Cavella <9434191+acavella@users.noreply.github.com>
2024-05-20 00:06:10 +0000

    update gitignore add function genrsa()

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/asn1"
 	"encoding/pem"
 	"flag"
 	"fmt"
@@ -30,26 +32,10 @@ func main() {
 	subject := flag.String("cn", "default", "string, defines request common name.")
 	rsaPtr := flag.Bool("rsa", false, "boolean, informs use of rsa cipher.")
 	eccPtr := flag.Bool("ecc", false, "boolean, informs use of ecc cipher.")
-
-	key, err := rsa.GenerateKey(rand.Reader, 4096)
-	if err != nil {
-		panic(err)
-	}
-	// Encode private key to PKCS#1 ASN.1 PEM.
-	keyPEM := pem.EncodeToMemory(
-		&pem.Block{
-			Type:  "RSA PRIVATE KEY",
-			Bytes: x509.MarshalPKCS1PrivateKey(key),
-		},
-	)
-
-	// Write private key to file.
-	if err := os.WriteFile("key.rsa", keyPEM, 0700); err != nil {
-		panic(err)
-	}
+	flag.Parse()
 
 	// Parse command line flags
-	flag.Parse()
+
 	keyUsage := viper.GetStringSlice("ku")
 	exkeyUsage := viper.GetStringSlice("eku")
 
@@ -65,10 +51,51 @@ func main() {
 
 	if *rsaPtr {
 		fmt.Println("Cipher: RSA")
+		genrsa(*subject)
 	} else if *eccPtr {
 		fmt.Println("Cipher: ECC")
 	} else {
 		fmt.Println("Cipher spec was not defined.")
 	}
 
+}
+
+func genrsa(subject string) {
+	// Build RDN
+	subj := pkix.Name{
+		CommonName:   subject,
+		Country:      []string{viper.GetString("dn.country")},
+		Province:     []string{viper.GetString("dn.state")},
+		Locality:     []string{viper.GetString("dn.city")},
+		Organization: []string{viper.GetString("dn.org")},
+	}
+	rawSubj := subj.ToRDNSequence()
+	asn1Subj, _ := asn1.Marshal(rawSubj)
+	template := x509.CertificateRequest{
+		RawSubject:         asn1Subj,
+		SignatureAlgorithm: x509.SHA384WithRSA,
+	}
+
+	key, err := rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		panic(err)
+	}
+
+	csrBytes, _ := x509.CreateCertificateRequest(rand.Reader, &template, key)
+
+	// Encode csr to PEM
+	csrPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE REQUEST", Bytes: csrBytes})
+
+	// Write private key to file.
+	if err := os.WriteFile("rsa.csr", csrPEM, 0755); err != nil {
+		panic(err)
+	}
+
+	// Encode private key to PKCS#1 ASN.1 PEM.
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
+
+	// Write private key to file.
+	if err := os.WriteFile("rsa.key", keyPEM, 0700); err != nil {
+		panic(err)
+	}
 }
